0de2e2b2244ef48d0da4c6e6e9d02846094161b4
PandaSekh <alessiofranceschi2@gmail.com>
2020-09-10 16:41:16 +0200

    un file si era rotto, boh

--- src/it/beije/oort/file/CsvReader.java
+++ src/it/beije/oort/file/CsvReader.java
@@ -3,11 +3,8 @@ package it.beije.oort.file;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-<<<<<<< HEAD
-=======
 import java.io.IOException;
 import java.util.StringTokenizer;
->>>>>>> refs/remotes/origin/master
 
 public class CsvReader {
 
@@ -35,7 +32,6 @@ public class CsvReader {
 		
 		while (bufferedReader.ready()) {
 			//System.out.println(bufferedReader.readLine());
-			@SuppressWarnings("unused")
 			String riga = bufferedReader.readLine();
 //			String[] campi = riga.split(";");
 //			
@@ -52,8 +48,6 @@ public class CsvReader {
 //			System.out.println("email : " + tokenizer.nextToken());
 
 			System.out.println();
-			
-			bufferedReader.close();
 		}
 	}
 
