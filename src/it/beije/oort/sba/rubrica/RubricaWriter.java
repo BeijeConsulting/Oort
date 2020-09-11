a9596c2dd100483d7791f493a3f8ac33a5fd5271
andreabrugaletta <andreabrugaletta@yahoo.it>
2020-09-11 17:21:21 +0200

    programma rubrica

--- src/it/beije/oort/sba/rubrica/RubricaWriter.java
+++ src/it/beije/oort/sba/rubrica/RubricaWriter.java
@@ -1,15 +1,13 @@
 package it.beije.oort.sba.rubrica;
-import java.io.BufferedWriter;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
 
 public class RubricaWriter {
 	
-	public void rubricaWriter(File file, int grandezzaRubrica, List nomi, List cognomi, List numeri, List email) throws IOException {
+	public void rubricaWriter(File file, int grandezzaRubrica, List<String> nomi, List<String> cognomi, List<String> numeri, List<String> email) throws IOException {
 		FileWriter writer = new FileWriter(file);
 		String line="";
 		ListRandomSelector nome = new ListRandomSelector(nomi);
@@ -17,22 +15,14 @@ public class RubricaWriter {
 		ListRandomSelector numero = new ListRandomSelector(numeri);
 		ListRandomSelector emails = new ListRandomSelector(email);
 		PhoneNumberGenerator sette = new PhoneNumberGenerator();
+		writer.write("NOME;COGNOME;NUMERO;E-MAIL;\n");
 		for(int i = 0;i<grandezzaRubrica;i++) {
 			String a = nome.getNext();
 			String b = cognome.getNext();
-			line+=a+";"+b+";"+numero.getNext()+ sette.nextNumber() + ";" + a +"." + b + "@"+ emails.getNext()+"\n";
+			line+=a+";"+b+";"+numero.getNext()+ sette.nextNumber() + ";" + a +"." + b + "@"+ emails.getNext()+ ";" + "\n";
 		}
 		writer.write(line);
-		
 		writer.flush();
 		writer.close();
 	}
-	
-
-
-	public static void main(String[] args) {
-
-
-	}
-
 }
