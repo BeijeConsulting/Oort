b2dfa810293efdf768ebc15c1a5d8ca5fbe0fc2b
h4rk <ale.sala.97@gmail.com>
2020-09-11 17:24:03 +0200

    Generatore rubrica completo di Alessandro Sala e Alessio Franceschi. Aggiunti anche file di input sanificati come si deve.

---
+++ src/it/beije/oort/file/rubrica/GeneratoreRubrica.java
@@ -0,0 +1,70 @@
+package it.beije.oort.file.rubrica;
+
+import java.io.*;
+import java.util.Random;
+import java.util.ArrayList;
+
+public class GeneratoreRubrica {
+		
+	private static String removeSpace(String s) {
+		if(s.indexOf(" ")!=-1) {	
+			StringBuilder sb = new StringBuilder(s);
+			s = sb.deleteCharAt(sb.indexOf(" ")).toString();
+		}
+		return s;
+	}
+	
+	private static String generaIdentita(String nome, String cognome,
+			String prefisso, String dominio) {
+		Random rr = new Random();
+		String suffisso="";
+		for(int i=0;i<7;i++) {
+			suffisso += rr.nextInt(10);
+		}
+		return "\""+nome+"\";\""+cognome+"\";\""+prefisso+suffisso+
+				"\";\""+GeneratoreRubrica.removeSpace(nome)+"."+
+				GeneratoreRubrica.removeSpace(cognome)+"@"+dominio+"\"\n";
+	}
+	
+	private static void writeRubrica(String filename, ArrayList<String> nomi, ArrayList<String> cognomi) 
+			throws IOException{
+		//sanificare filename
+		Random r = new Random();
+		String[] prefissi = {"345", "346", "347", "348","349"};
+		String[] domini = {"gmail.com", "hotmail.com", "hotmail.it", 
+				"libero.it", "yahoo.com", "virgilio.it", "tim.it", "alice.it"};
+		//try-catch per IOException
+		BufferedWriter bf = new BufferedWriter(new FileWriter(filename, true));
+		bf.write("NOME;COGNOME;TELEFONO;EMAIL;\n");
+		for(int i=0;i<1000;i++) {
+			bf.write(GeneratoreRubrica.generaIdentita(nomi.get(r.nextInt(nomi.size())),
+					cognomi.get(r.nextInt(cognomi.size())), prefissi[r.nextInt(5)],
+					domini[r.nextInt(8)]));
+		}
+		bf.close();
+	}
+	
+	private static ArrayList<String> getListFromFile(String path) throws IOException{
+		BufferedReader br = new BufferedReader(new FileReader(path));
+		ArrayList<String> valori = new ArrayList<>();
+		while (br.ready()) {
+			valori.add(br.readLine());
+		}
+		br.close();
+		return valori;
+	}
+	
+	public static void generaRubrica(String filename, String pathNomi, String pathCognomi) 
+			throws IOException{
+		ArrayList<String> nomi = GeneratoreRubrica.getListFromFile(pathNomi);
+		ArrayList<String> cognomi = GeneratoreRubrica.getListFromFile(pathCognomi);
+		GeneratoreRubrica.writeRubrica(filename, nomi, cognomi);
+	}
+	
+	public static void main(String[] args) throws IOException{	
+		GeneratoreRubrica.generaRubrica("C:/Users/Padawan12/Desktop/rubrica.txt",
+				"C:/Users/Padawan12/Desktop/nomi.txt",
+				"C:/Users/Padawan12/Desktop/cognomi.txt");
+	}
+
+}
