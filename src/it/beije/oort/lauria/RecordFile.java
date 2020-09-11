d8fee52d3037971d9f6a4afb0716ca3d077867f5
Padawan06 <Padawan06@DESKTOP-9RDOHCL.station>
2020-09-11 17:07:23 +0200

    commit per giuseppe

--- src/it/beije/oort/lauria/RecordFile.java
+++ src/it/beije/oort/lauria/RecordFile.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class RecordFile {
 		
 	private static final String PATH_FILES = "C:\\Users\\Padawan06\\Documenti\\temp\\";
@@ -11,8 +12,7 @@ public class RecordFile {
 	// lettura file
 	public static String getContent(File file) throws IOException {
 		FileReader fileReader = new FileReader(file);
-		StringBuilder builder = new StringBuilder();
-		
+		StringBuilder builder = new StringBuilder();		
 		BufferedReader bufferedReader = new BufferedReader(fileReader);
 		
 		while (bufferedReader.ready()) {
@@ -23,12 +23,10 @@ public class RecordFile {
 	}
 
 	// lettura e memorizzazione record	
-	public static List<String> memContent(File file, List<String> record ) throws IOException {
-			
-		FileReader fileReader = new FileReader(file);
-			
-		BufferedReader bufferedReader = new BufferedReader(fileReader);
-			
+	public static List<String> memContent(File file, List<String> record ) throws IOException {			
+		FileReader fileReader = new FileReader(file);			
+		BufferedReader bufferedReader = new BufferedReader(fileReader);			
+		
 		while (bufferedReader.ready()) {
 			record.add(bufferedReader.readLine());
 		}
@@ -41,26 +39,19 @@ public class RecordFile {
 		File fileNomi = new File(PATH_FILES + "nomi_italiani.txt");
 		File fileCognomi = new File(PATH_FILES + "cognomi.txt");
 		
-		// lettura e memorizzazione dei record
+		// Dichiarazione variabili 
 		List<String> recordNomitemp = new ArrayList<>();
 		List<String> recordCognomitemp = new ArrayList<>();
+		
 		List<String> recordNomi = new ArrayList<>();
-		List<String> recordCognomi = new ArrayList<>();
+		List<String> recordCognomi = new ArrayList<>();	
+		List<String> recordTel = new ArrayList<>();
+		List<String> recordMail = new ArrayList<>();
 		
-//		List<String> recordPrefissi = new ArrayList<>();
-//		List<String> recordTel = new ArrayList<>();
-//		List<String> recordMail = new ArrayList<>();
-		
+		// lettura e memorizzazione dei record di nomi e cognomi
 		recordNomitemp = RecordFile.memContent(fileNomi, recordNomitemp);
 		recordCognomitemp = RecordFile.memContent(fileCognomi, recordCognomitemp);
-		
-//		recordNomi = RecordFile.memContent(fileNomi, recordNomi);
-//		recordNomi = RecordFile.memContent(fileNomi, recordNomi);
-//		recordNomi = RecordFile.memContent(fileNomi, recordNomi);
-		
-		System.out.println("numero nomi disponibili: "+recordNomitemp.size());
-		System.out.println("numero cognomi disponibili: "+recordCognomitemp.size());
-		
+
 		int i;
 		int j; 
 		
@@ -69,24 +60,25 @@ public class RecordFile {
 			j = (int) (Math.random()*recordCognomitemp.size());			
 			recordNomi.add(recordNomitemp.get(i));
 			recordCognomi.add(recordCognomitemp.get(j));
-			
-			
+			recordTel.add(RecordFile.generaNumero());
+			recordMail.add(RecordFile.generaMail(recordNomi.get(k), recordCognomi.get(k)));
 		}
-		System.out.println("numero nomi disponibili : "+recordNomi.size());
-		System.out.println("numero cognomi disponibili : "+recordCognomi.size());
+		
+		
 		
 		// fase di scrittura	
 		File output = new File(PATH_FILES + "rubrica.txt");
 		FileWriter writer = new FileWriter(output);
 		
-		boolean isPrimaRiga = true;
-		for (int arrayIndex = 0; arrayIndex < 1000; arrayIndex++) {
-			if (isPrimaRiga) {
-				writer.write("\n");
-				isPrimaRiga = false;
-			}
-			// TODO mettere variabili
-			// RecordFile.aggiungiRiga(writer, nome, cognome, numero, email);
+		writer.write("NOME;COGNOME;TELEFONO;E-MAIL");
+		for (int arrayIndex = 0; arrayIndex < recordNomi.size(); arrayIndex++) {
+			writer.write("\n");
+			RecordFile.aggiungiRiga(
+					writer,
+					recordNomi.get(arrayIndex),
+					recordCognomi.get(arrayIndex),
+					recordTel.get(arrayIndex),
+					recordMail.get(arrayIndex));
 		}
 		
 		writer.flush();
@@ -101,91 +93,36 @@ public class RecordFile {
 		writer.write(riga.toString());
 		return writer;
 	}
+	
+	// genera Numero di telefono
+	private static String generaNumero() {
+		String[] prefissi = {"345", "346", "347", "348", "349"};
+		StringBuilder s = new StringBuilder();
+		
+		int indexPrefisso = (int) (Math.random() * prefissi.length);
+		s.append(prefissi[indexPrefisso]);
+		
+		for(int i = 0; i < 7; i++) {
+			s.append((int) (Math.random() * 9));
+		}
+		
+		return s.toString();
+	}
+	
+	private static String generaMail(String nome, String cognome) {
+		String[] dominio = {"gmail.com", "hotmail.com", "hotmail.it", "libero.it", "yahoo.com", "virgilio.it", "tim.it", "alice.it"};
+		StringBuilder s = new StringBuilder();
+		
+		nome = nome.replace(" ", "").replace("'", "");
+		cognome = cognome.replace(" ", "").replace("'", "");
+		
+		s.append(nome.toLowerCase()).append('.').append(cognome.toLowerCase()).append('@');
+		
+		int indexDominio = (int) (Math.random() * dominio.length);
+		s.append(dominio[indexDominio]);
+		
+		return s.toString();
+		
+	}
 
 }
-
-
-//
-//
-//
-//import java.io.BufferedReader;
-//import java.io.File;
-//import java.io.FileNotFoundException;
-//import java.io.FileReader;
-//import java.io.FileWriter;
-//import java.io.IOException;
-//import java.util.Arrays;
-//import java.util.Scanner;
-//
-//import it.beije.oort.file.CsvReader;
-//
-//public class ReadAndWrite {
-//	
-//	
-//	
-//	public static String getContent(File file) throws IOException {
-//		FileReader fileReader = new FileReader(file);
-//		StringBuilder builder = new StringBuilder();
-//		
-//		BufferedReader bufferedReader = new BufferedReader(fileReader);
-//		
-//		while (bufferedReader.ready()) {
-//			builder.append(bufferedReader.readLine()).append('\n');
-//		}
-//		
-//		return builder.toString();
-//	}
-//	
-//	public static void writeRow(File file, String content) throws IOException {
-//		FileWriter writer = new FileWriter(file);
-//		
-//		writer.write(content);
-//		
-//		writer.flush();
-//		writer.close();
-//	}
-//	
-//
-//	public static void main(String[] args) throws IOException {
-//		
-//		File file = new File("C:\\Users\\Padawan06\\Documenti\\prova.txt");
-//		
-//		System.out.println("esiste ? "+file.exists());
-//		
-//		String content = "";
-//		
-//		if (file.exists()) {
-//			System.out.println("esiste, lo carico... ");
-//			
-//			content = CsvReader.getContent(file);
-//			System.out.println(content);
-//			
-//		} else {
-//			System.out.println("non esiste");
-//		}
-//
-//		
-//		
-//		Scanner s = new Scanner(System.in);
-//		String st = s.nextLine();
-//		
-//		
-//		while (!st.equalsIgnoreCase("Q")) {
-//			
-//			content = content + '\n' + st;
-//			writeRow(file, content);
-//			System.out.println(st);
-//			st = s.nextLine();
-//			
-//		}
-//		
-//		
-//		
-//		System.gc();
-//		System.out.println("BYE!!");
-//		s.close();
-//
-//
-//	}
-//
-//}
