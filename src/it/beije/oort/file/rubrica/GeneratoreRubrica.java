2e18c871bafa618c6e6fd9f2a2d87f5a61ab29a7
h4rk <ale.sala.97@gmail.com>
2020-09-14 16:34:22 +0200

    Generatore rubrica terminato + bonus track

--- src/it/beije/oort/file/rubrica/GeneratoreRubrica.java
+++ src/it/beije/oort/file/rubrica/GeneratoreRubrica.java
@@ -7,11 +7,135 @@ import java.util.ArrayList;
 public class GeneratoreRubrica {
 		
 	private static String removeSpace(String s) {
-		if(s.indexOf(" ")!=-1) {	
-			StringBuilder sb = new StringBuilder(s);
-			s = sb.deleteCharAt(sb.indexOf(" ")).toString();
+		StringBuilder sb = new StringBuilder(s);
+		while(sb.indexOf(" ")!=-1) {
+			sb.deleteCharAt(sb.indexOf(" "));
 		}
-		return s;
+		return sb.toString();
+	}
+	
+	private static String removeSingleQuote(String s) {
+		StringBuilder sb = new StringBuilder(s);
+		while(sb.indexOf("'")!=-1) {
+			sb.deleteCharAt(sb.indexOf("'"));
+		}
+		return sb.toString();
+	}
+	
+	static boolean usaNome() {
+		Random r = new Random();
+		int v = r.nextInt(5);
+		return (v == 1) ? false : true;
+	}
+	
+	static boolean usaCognome() {
+		Random r = new Random();
+		int v = r.nextInt(3);
+		return (v == 1) ? false : true;
+	}
+	
+	private static String abbreviaVocale(String s) {
+		StringBuilder temp = new StringBuilder();
+		temp.append(s.charAt(0));
+		for (int i = 1; i < s.length(); i++) {
+			if ("AEIOUaeiou".indexOf(s.charAt(i)) != -1) {
+				temp.append(s.charAt(i));
+				break;
+			}
+			temp.append(s.charAt(i));
+		}
+		return temp.toString();
+	}
+	
+	private static String sostituisciVocali(String s) {
+		StringBuilder temp = new StringBuilder();
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			if (c == 'a' || c == 'A') {
+				temp.append(4);
+			} else if (c == 'e' || c == 'E') {
+				temp.append(3);
+			} else if (c == 'i' || c == 'I') {
+				temp.append(1);
+			} else if (c == 'o' || c == 'O') {
+				temp.append(0);
+			} else {
+				temp.append(c);
+			}
+		}
+		return temp.toString();
+	}
+
+	private static String mailNomeCognome(String nome, String cognome, String dominio) {
+		Random r = new Random();
+		StringBuilder email = new StringBuilder();
+		
+		// random 1-10 : valori 1-2 abbreviare il nome alla sola iniziale; 
+		int abbrevia = r.nextInt(9) + 1;
+		if (abbrevia <= 2) {
+			nome = nome.substring(0,1);
+		} else if (abbrevia <= 5) {
+		// 3-5 abbreviare fino alla prima vocale successiva alla prima lettera (compresa)
+			nome = abbreviaVocale(nome);
+		}
+		
+		/*
+		 *  separatore deciso da ulteriore random 1-10:
+			 1-3  nessuno
+			 3-7  . (punto)
+			 8-10 - (trattino) (edited) 
+		 */
+		String separatore = "";
+		int rSeparatore = r.nextInt(10) + 1;
+		if(rSeparatore > 3 && rSeparatore <= 7) {
+			separatore = ".";
+		} else if( rSeparatore <= 10) {
+			separatore = "-";
+		}
+		
+		if (r.nextInt(5) + 1 == 1) {
+			nome = sostituisciVocali(nome);
+			cognome = sostituisciVocali(cognome);
+		}
+		
+		// - random 1-4 : se esce 1 invertire nome con cognome
+		if ((r.nextInt(3)+1) == 1) {
+			email.append(cognome).append(separatore).append(nome);
+		} else {
+			email.append(nome).append(separatore).append(cognome);
+		}
+		
+		//Unisco
+		return GeneratoreRubrica.removeSpace(email.append("@")
+				.append(dominio).toString().toLowerCase());
+}
+	
+	private static String generaMail(String nome, String cognome, String dominio) {
+		String email = "";
+		Random r = new Random();
+		int rand = r.nextInt(10);
+		boolean usaNome = GeneratoreRubrica.usaNome();
+		boolean usaCognome = GeneratoreRubrica.usaCognome();
+		
+		if (!usaNome && !usaCognome) {
+			for(int i=0;i<r.nextInt(15)+6;i++) {
+				email += r.nextInt(10);
+			}
+			email += "@"+dominio;
+		} else if (!usaNome) {
+			if (rand != 1) email = GeneratoreRubrica.removeSpace(cognome) + r.nextInt(10) +
+					r.nextInt(10) + "@" + dominio;
+			else email = GeneratoreRubrica.removeSpace(cognome) + "@" + dominio;
+		} else if (!usaCognome){
+			if (rand != 1) email = GeneratoreRubrica.removeSpace(nome) + r.nextInt(10) +
+					r.nextInt(10) + "@" + dominio;
+			else email = GeneratoreRubrica.removeSpace(nome) + "@" + dominio;
+		} else {
+			email = GeneratoreRubrica.mailNomeCognome(nome, cognome, dominio);
+			//bonus track goes here
+		}
+		
+		return GeneratoreRubrica.removeSingleQuote(email).toLowerCase();
 	}
 	
 	private static String generaIdentita(String nome, String cognome,
@@ -21,12 +145,17 @@ public class GeneratoreRubrica {
 		for(int i=0;i<7;i++) {
 			suffisso += rr.nextInt(10);
 		}
+
 		return "\""+nome+"\";\""+cognome+"\";\""+prefisso+suffisso+
-				"\";\""+GeneratoreRubrica.removeSpace(nome)+"."+
-				GeneratoreRubrica.removeSpace(cognome)+"@"+dominio+"\"\n";
+				"\";\""+GeneratoreRubrica.generaMail(nome, cognome, dominio)+"\"\n";
+		
+//		return "\""+nome+"\";\""+cognome+"\";\""+prefisso+suffisso+
+//		"\";\""+GeneratoreRubrica.removeSpace(nome)+"."+
+//		GeneratoreRubrica.removeSpace(cognome)+"@"+dominio+"\"\n";
 	}
 	
-	private static void writeRubrica(String filename, ArrayList<String> nomi, ArrayList<String> cognomi) 
+	private static void writeRubrica(String filename, ArrayList<String> nomi,
+			ArrayList<String> cognomi) 
 			throws IOException{
 		//sanificare filename
 		Random r = new Random();
@@ -68,32 +197,11 @@ public class GeneratoreRubrica {
 	}
 	
 	
-	private static String generaMail(String nome, String cognome, String dominio) {
-		String email = "";
-		Random r = new Random();
-		int rand = r.nextInt(10);
-		if (!usaNome() && !usaCognome()) {
-			for(int i =0;i<r.nextInt(16);i++) {
-				
-			}
-		} else if (!usaNome()) {
-			if (rand != 1) email = GeneratoreRubrica.removeSpace(cognome) + r.nextInt(10) + r.nextInt(10) + "@" + dominio;
-			else email = GeneratoreRubrica.removeSpace(cognome) + "@" + dominio;
-		} else if (!usaCognome()){
-			if (rand != 1) email = GeneratoreRubrica.removeSpace(nome) + r.nextInt(10) + r.nextInt(10) + "@" + dominio;
-			else email = GeneratoreRubrica.removeSpace(nome) + "@" + dominio;
-		}
-	}
 	
-	static boolean usaNome() {
-		Random r = new Random();
-		int v = r.nextInt(5);
-		return (v == 1) ? false : true;
-	}
 	
-	static boolean usaCognome() {
-		Random r = new Random();
-		int v = r.nextInt(3);
-		return (v == 1) ? false : true;
-	}
+	
+	
+	
+	
+	
 }
