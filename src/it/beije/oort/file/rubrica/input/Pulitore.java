ef2bf8d923ba836408c7162ebd23c8b4fdffb62e
PandaSekh <alessiofranceschi2@gmail.com>
2020-09-14 16:47:13 +0200

    Pulito e sistemato il codice del pulitore

--- src/it/beije/oort/file/rubrica/input/Pulitore.java
+++ src/it/beije/oort/file/rubrica/input/Pulitore.java
@@ -10,47 +10,43 @@ import java.util.ArrayList;
  * Classe utility per sanificare gli input prima dell'utilizzo.
  * Rimuove spazi in eccesso, caratteri speciali ed eventuali caratteri pre e post parola.
  */
-public class Pulitore {
 
+public class Pulitore {
 	public static void main(String[] args) throws Exception{
 		String fileNomi = "\\nomi.txt";
 		String fileCognomi = "\\cognomi.txt";
 		
-		ArrayList<String> nomi = getValues(fileNomi);
-		ArrayList<String> cognomi = getValues(fileCognomi);
-		
-		writeToFile(nomi, fileNomi);
-		writeToFile(cognomi, fileCognomi);
+		writeToFile(getValues(fileNomi), fileNomi);
+		writeToFile(getValues(fileCognomi), fileCognomi);
 	}
 	
-	static ArrayList<String> getValues(String filePath ) throws Exception {
+	private static ArrayList<String> getValues(String filePath) throws Exception {
 		BufferedReader br = new BufferedReader(new FileReader(filePath));
 		ArrayList<String> valori = new ArrayList<>();
 		while (br.ready()) {
 			String t = br.readLine();
-			 valori.add(allUp(t));
+			valori.add(correctUpperCase(t));
 		}
 		br.close();
-		System.out.println("File letto.");
+		System.out.println("File " + filePath + " letto.");
 		return valori;
 	}
 	
-	static void writeToFile(ArrayList<String> valori, String filePath) throws Exception {
+	private static void writeToFile(ArrayList<String> valori, String filePath) throws Exception {
 		BufferedWriter bw = new BufferedWriter(new FileWriter(filePath));
 		for (String s : valori) {
 			bw.write(s);
 			bw.newLine();
-			}
+		}
 		bw.flush();
 		bw.close();
-		System.out.println("File pulito.");
+		System.out.println("File " + filePath + " pulito.");
 	} 
 	
-	static String allUp(String s) {
-		s = s.trim().replaceAll(" +", " ").replaceAll("[-+.^:,]","");;
-		String[] arr = s.split(" ");
+	private static String correctUpperCase(String s) {
+		String[] arr = s.trim().replaceAll(" +", " ").replaceAll("[-+.^:,]","").split(" ");
 		StringBuilder sb = new StringBuilder();
-		for (int i=0; i<arr.length; i++) {
+		for (int i = 0; i < arr.length; i++) {
 			arr[i] = arr[i].substring(0, 1).toUpperCase() + arr[i].substring(1);
 			sb.append(arr[i] + ' ');
 		}
