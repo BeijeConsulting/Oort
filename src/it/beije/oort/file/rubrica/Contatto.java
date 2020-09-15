8871694ff391500327e31ceed4fd7f7a509aeeff
PandaSekh <alessiofranceschi2@gmail.com>
2020-09-15 15:16:00 +0200

    usato StringBuilder

--- src/it/beije/oort/file/rubrica/Contatto.java
+++ src/it/beije/oort/file/rubrica/Contatto.java
@@ -50,6 +50,11 @@ public class Contatto {
 	 * @return the formatted string following the csv convention with quotes.
 	 */
 	public String toFormattedString() {
-		return "\""+nome+"\";\""+cognome+"\";\""+cell+"\";\""+email+"\""; //TODO: fare con StringBuilder
+		//return "\""+nome+"\";\""+cognome+"\";\""+cell+"\";\""+email+"\""; //TODO: fare con StringBuilder
+		return new StringBuilder("\"")
+				.append(nome)
+				.append("\";\"")
+				.append(cognome)
+				.append("\";\"").append(cell).append("\";\"").append(email).append("\"").toString();
 	}
 }
