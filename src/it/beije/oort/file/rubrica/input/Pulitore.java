aa8914c42e7009b6d8061b9a9054e19ff7434c2f
PandaSekh <alessiofranceschi2@gmail.com>
2020-09-15 14:01:27 +0200

    Aggiunta documentazione per il Pulitore

--- src/it/beije/oort/file/rubrica/input/Pulitore.java
+++ src/it/beije/oort/file/rubrica/input/Pulitore.java
@@ -6,11 +6,13 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.ArrayList;
 
-/*
+/**
  * Classe utility per sanificare gli input prima dell'utilizzo.
  * Rimuove spazi in eccesso, caratteri speciali ed eventuali caratteri pre e post parola.
+ * 
+ * @author Alessio Franceschi e Alessandro Sala
+ * @version 1.0
  */
-
 public class Pulitore {
 	public static void main(String[] args) throws Exception{
 		String fileNomi = "\\nomi.txt";
@@ -19,19 +21,29 @@ public class Pulitore {
 		writeToFile(getValues(fileNomi), fileNomi);
 		writeToFile(getValues(fileCognomi), fileCognomi);
 	}
-	
+	/**
+	 * Read values from a file divided by new line and puts them into an ArrayList<String>. 
+	 * @param filePath The path of the file to be cleaned.
+	 * @return An ArrayList<String> of the input file values divided by new line.
+	 * @throws Exception
+	 */
 	private static ArrayList<String> getValues(String filePath) throws Exception {
 		BufferedReader br = new BufferedReader(new FileReader(filePath));
 		ArrayList<String> valori = new ArrayList<>();
 		while (br.ready()) {
 			String t = br.readLine();
-			valori.add(correctUpperCase(t));
+			valori.add(clean(t));
 		}
 		br.close();
 		System.out.println("File " + filePath + " letto.");
 		return valori;
 	}
-	
+	/**
+	 * Write to file.
+	 * @param valori The cleaned values to be written on the file.
+	 * @param filePath The path of the file to be written.
+	 * @throws Exception
+	 */
 	private static void writeToFile(ArrayList<String> valori, String filePath) throws Exception {
 		BufferedWriter bw = new BufferedWriter(new FileWriter(filePath));
 		for (String s : valori) {
@@ -41,9 +53,13 @@ public class Pulitore {
 		bw.flush();
 		bw.close();
 		System.out.println("File " + filePath + " pulito.");
-	} 
-	
-	private static String correctUpperCase(String s) {
+	} 	
+	/**
+	 * Clean a String by removing special characters and by converting double spaces to single spaces.
+	 * @param s String to clean.
+	 * @return String cleaned.
+	 */
+	private static String clean(String s) {
 		String[] arr = s.trim().replaceAll(" +", " ").replaceAll("[-+.^:,]","").split(" ");
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < arr.length; i++) {
