7722906478e8bbd7887733494906564d151006d1
PandaSekh <alessiofranceschi2@gmail.com>
2020-09-16 14:44:26 +0200

    Null

--- src/it/beije/oort/franceschi/rubrica/Main.java
+++ src/it/beije/oort/franceschi/rubrica/Main.java
@@ -7,6 +7,7 @@ public class Main {
 	private static String fileNomi = "\\Code\\Oort\\cose\\nomi.txt";
 	private static String fileCognomi = "\\Code\\Oort\\cose\\nomi.txt";
 	
+	@SuppressWarnings("unused")
 	public static void main(String[] args) throws Exception {
 		String[] nomi = Lettore.getArrayValori(fileNomi);
 		String[] cognomi = Lettore.getArrayValori(fileCognomi);
