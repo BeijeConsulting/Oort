507667546b3a0dfb57be8a4c9d079547e796b364
Padawan04 <Padawan04@DESKTOP-CJ91L3V.station>
2020-09-17 13:01:38 +0200

    commit rubrica da csv a xml

---
+++ src/it/beije/oort/rubrica/Contatto.java
@@ -0,0 +1,61 @@
+package it.beije.oort.rubrica;
+
+public class Contatto {
+	
+	private String nome;
+	private String cognome;
+	private String telefono;
+	private String email;
+	
+	public Contatto() {}
+	
+	public Contatto(String nome, String cognome, String telefono) {
+		this(nome, cognome, telefono, "");
+	}
+	
+	public Contatto(String nome, String cognome, String telefono, String email) {
+		this.nome = nome;
+		this.cognome = cognome;
+		this.telefono = telefono;
+		this.email = email;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append(this.nome).append(';')
+			.append(this.cognome).append(';')
+			.append(this.telefono).append(';')
+			.append(this.email).append(';');
+		
+		return builder.toString();
+	}
+	
+}
