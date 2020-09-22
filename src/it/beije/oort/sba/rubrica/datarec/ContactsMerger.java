4bf89f5c3d5a4974dc97328c806c77d0889d4892
Brando <b.mancuso37@gmail.com>
2020-09-22 17:26:13 +0200

    i nani

--- src/it/beije/oort/sba/rubrica/datarec/ContactsMerger.java
+++ src/it/beije/oort/sba/rubrica/datarec/ContactsMerger.java
@@ -1,8 +1,5 @@
 package it.beije.oort.sba.rubrica.datarec;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -10,6 +7,7 @@ import java.util.Set;
 import it.beije.oort.sba.rubrica.Contatto;
 
 public class ContactsMerger {
+	
 	public static List<Contatto> composeList(ContactsMap map) {
 		List<Contatto> contatti = new ArrayList<Contatto>(2000);
 		Set<Contatto> setContatti;
