3565a261dee8d45937da458ddd6b4e89317e511c
Luca Gregori <lucagregori96@gmail.com>
2020-09-22 18:00:31 +0200

    TODO: fix null in print

--- src/it/beije/oort/gregori/rubrica/hashmap/Duplicates.java
+++ src/it/beije/oort/gregori/rubrica/hashmap/Duplicates.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.List;
 
 import it.beije.oort.rubrica.Contatto;
-import it.beije.oort.gregori.rubrica.parser.*;
+import it.beije.oort.gregori.rubrica.parser.ReaderCsv;
 
 public class Duplicates {
 	
@@ -15,22 +15,83 @@ public class Duplicates {
 		HashMap<String, List<Contatto>> hm = new HashMap<>();	
 		List<Contatto> returnList = new ArrayList<>();
 		for(Contatto contatto : contatti) {
-			if(hm.containsKey(contatto.getTelefono())) {
-				for(int i = 0; i < hm.get(contatto.getTelefono()).size(); i++) {
-					if(!hm.get(contatto.getTelefono()).get(i).equals(contatto)) {
+			// Se il numero di telefono non è vuoto
+			if(!contatto.getTelefono().equals("")) {
+				// Se la mappa contiene una chiave telfono già esistente
+				if(hm.containsKey(contatto.getTelefono())) {
+					// Se i contatti con la stessa chiave sono diversi 
+					// aggiungili alla lista contatti nella mappa
+					if(!hm.get(contatto.getTelefono()).contains(contatto)) {
 						hm.get(contatto.getTelefono()).add(contatto);
 					}
 				}
+				else {
+					List<Contatto> temp = new ArrayList<>();
+					temp.add(contatto);
+					hm.put(contatto.getTelefono(), temp);
+				}
 			}
-			else {
-				List<Contatto> temp = new ArrayList<>();
-				temp.add(contatto);
-				hm.put(contatto.getTelefono(), temp);
-			}
-			
 		}
 		
-		myPrint(hm);
+		for(List<Contatto> listContatti : hm.values()) {
+			List<String> setNomi = new ArrayList<>();
+			List<String> setCognomi = new ArrayList<>();
+			List<String> setEmails = new ArrayList<>();	
+			String telefono = "";
+			if(listContatti.size() > 1) {
+				for(Contatto contatto : listContatti) {
+					if(!setNomi.contains(contatto.getNome())) setNomi.add(contatto.getNome());
+					if(!setCognomi.contains(contatto.getCognome())) setCognomi.add(contatto.getCognome());
+					if(!setEmails.contains(contatto.getEmail())) setEmails.add(contatto.getEmail());
+					telefono = contatto.getTelefono();
+				}
+//				System.out.println(setNomi);
+//				System.out.println(setCognomi);
+//				System.out.println(setEmails);
+//				System.out.println("---------------");
+				
+				Contatto contatto = new Contatto();
+				List<String> trash = new ArrayList<>();
+				boolean flag = true;
+				
+				for(int i = 0; i < setNomi.size(); i++) {
+					if(!setNomi.get(i).equals("") && flag) {
+						contatto.setNome(setNomi.get(i));
+						flag = false;
+					}
+					else if(!setNomi.get(i).equals("")){
+						trash.add(setNomi.get(i));
+					}
+				}
+				
+				flag = true;
+				for(int i = 0; i < setCognomi.size(); i++) {
+					if(!setCognomi.get(i).equals("") && flag) {
+						contatto.setCognome(setCognomi.get(i));
+						flag = false;
+					}
+					else if(!setCognomi.get(i).equals("")){
+						trash.add(setCognomi.get(i));
+					}
+				}
+				
+				flag = true;
+				for(int i = 0; i < setEmails.size(); i++) {
+					if(!setEmails.get(i).equals("") && flag) {
+						contatto.setEmail(setEmails.get(i));
+						flag = false;
+					}
+					else if(!setEmails.get(i).equals("")){
+						trash.add(setEmails.get(i));
+					}
+				}
+				
+				contatto.setTelefono(telefono);
+				returnList.add(contatto);
+			}
+		}
+		
+//		myPrint(hm);
 		
 		return returnList;
 	}
@@ -44,8 +105,9 @@ public class Duplicates {
 	public static void main(String[] args) throws IOException {
 		File csvFile = new File("/temp/rubrica.csv");
 		List<Contatto> contatti = ReaderCsv.readContatti(csvFile);
-		File pathFile = new File("duplicates_removed.csv");
+		File pathFile = new File("/temp/duplicates_removed.csv");
 		Duplicates.checkDuplicates(contatti, pathFile);
+		WriterCsv.writeContatti(Duplicates.checkDuplicates(contatti, pathFile), pathFile);
 	}
 	
 }
