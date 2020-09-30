9ff1eb887cfb51bc4774098a0b84dbd10a2389d4
h4rk <ale.sala.97@gmail.com>
2020-09-30 17:46:34 +0200

    Aggiunto commento esplicativo su Databasable

--- src/it/beije/oort/file/sala/biblioteca/Databasable.java
+++ src/it/beije/oort/file/sala/biblioteca/Databasable.java
@@ -1,5 +1,6 @@
 package it.beije.oort.file.sala.biblioteca;
 
 public interface Databasable {
-
+	//Interface implemented from all javaBeans in 'Biblioteca' so that methods that read and
+	//write on DB can be generalized and handled by a common List<Databasable> list.
 }
