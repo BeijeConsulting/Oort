476e7914ca7170251c4367a7c133a2d277f00ffb
Davide Girardi <gyra-96@hotmail.it>
2020-10-08 17:44:30 +0200

    agiunto runner...

--- src/it/beije/oort/girardi/Runner.java
+++ src/it/beije/oort/girardi/Runner.java
@@ -1,10 +1,12 @@
 package it.beije.oort.girardi;
 
+import java.time.LocalDate;
+
 public class Runner {
 
 	public static void main(String[] args) {
-		
-
+		LocalDate date = LocalDate.of(1877, 7, 22);
+		System.out.println(date);
 	}
 
 }
